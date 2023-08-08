334443658ae3365976ea952a2a3196ce1920aed0
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2023-08-08 17:58:56 -0500

    add placeholders to inputs

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -5,8 +5,8 @@ const Meme = () => {
         <>
             <form className="form">
                 <div className="input-form">
-                    <input type="text" className="input-text"></input>
-                    <input type="text" className="input-text"></input>
+                    <input type="text" className="input-text" placeholder='Top Message'></input>
+                    <input type="text" className="input-text" placeholder='Bottom Message'></input>
                 </div>
                 <button>Get a new meme image</button>
                 <div className="display-image">
