854119fff8402cdbf8fa4dd4434d60ebfd1366c1
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2023-08-09 18:27:33 -0500

    worked on inputs in form to save data to useState

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,14 +1,48 @@
+import { useState } from 'react'
 import './Meme.css'
 
 const Meme = () => {
+    const [formData, setFormData] = useState({
+        topMessage: '', bottomMessage:''
+    })
+
+    const changeHandler = (event) => {
+        console.log(event.target.value)
+        setFormData(prevState => {
+            return {
+                    ...prevState,
+                    [event.target.name]: event.target.value 
+                 }})
+    } 
+
+    const clickHandle = (event) => {
+        event.preventDefault()
+        console.log(formData.topMessage)
+        console.log(formData.bottomMessage)
+    }
+
     return (
         <>
             <form className="form">
                 <div className="input-form">
-                    <input type="text" className="input-text" placeholder='Top Message'></input>
-                    <input type="text" className="input-text" placeholder='Bottom Message'></input>
+                    <input 
+                        type="text" 
+                        className="input-text" 
+                        placeholder='Top Message'
+                        onChange={changeHandler} 
+                        name='topMessage'
+                        value={formData.topMessage}
+                    />
+                    <input 
+                        type="text" 
+                        className="input-text" 
+                        placeholder='Bottom Message'
+                        onChange={changeHandler}
+                        name='bottomMessage'
+                        value={formData.bottomMessage}
+                    />
                 </div>
-                <button>Get a new meme image</button>
+                <button onClick={clickHandle}>Get a new meme image</button>
                 <div className="display-image">
 
                 </div>
